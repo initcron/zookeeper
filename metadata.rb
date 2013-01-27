b1d173f94c311050ef7b97721449fff4696437c5
Gourav Shah <gs@initcron.org>
2013-01-27 17:15:34 +0530

    bumping version

--- metadata.rb
+++ metadata.rb
@@ -3,7 +3,7 @@ maintainer_email "gs@initcron.org"
 license          "Apache 2.0"
 description      "Installs/Configures zookeeper"
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.rdoc'))
-version          "0.1"
+version          "1.0"
 
 recipe "zookeeper::ebs_volume", "Attaches or creates an EBS volume for zookeeper"
 
